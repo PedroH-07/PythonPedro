26c770391d1a2f0160c391363eefbea13306eec4
PedroH-07 <phscardoso1@gmail.com>
2025-04-04 08:55:41 -0300

    primeiro comit em python

---
+++ aula01-variaveis-operadores/aula01c-operadores-relacionais.py
@@ -0,0 +1,23 @@
+print(6 == 2)
+print(6 != 2)
+
+idade = 20
+
+print(idade == 10)
+print(idade != 10)
+print(idade >= 18)
+
+logado = True
+
+if logado:
+    print('entrar no programa')
+
+maior_idade = idade >= 18
+
+if maior_idade:
+    print('vc é maior de idade')
+
+nome1 = "Marcos"
+nome2 = "marcos"
+
+print(nome1 == nome2)
